b4d9c05105309836db97e7dbeb9af15bb57286c9
tuanpcl7103 <tuanpcl7103.gmail.com>
2025-01-14 21:01:34 +0700

    [UPDATE]: Landing section 1 & Landing section 2

--- Front-end/src/pages/System/Landing.tsx
+++ Front-end/src/pages/System/Landing.tsx
@@ -1,9 +1,68 @@
 export default function () {
     return (
-        <main id="landing">
-            <section id='landing-s1' className="landing-section">
+        <>
+            <section id='landing-s1'>
             </section>
-        </main>
+            <section id="landing-s2" className="landing-section">
+                <div className="ls2-container">
+                    <h2>Chiến dịch từ thiện nổi bật</h2>
+                    <div className="ls2r1">
+                        <h3>Chiến dịch của tổ chức</h3>
+                        <div className="campaign-lists">
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                        </div>
+                        <div className="pagination-lists">
+                            <span className="pagination-item pagination-item-active"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                        </div>
+                    </div>
+                    <div className="ls2r2">
+                        <h3>Chiến dịch của cá nhân</h3>
+                        <div className="campaign-lists">
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                            <div className="campaign-card">
+                                <div className="campaign-img"></div>
+                                <h4>Tiêu đề</h4>
+                                <p>Ngày hết hạn</p>
+                            </div>
+                        </div>
+                        <div className="pagination-lists">
+                            <span className="pagination-item pagination-item-active"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                            <span className="pagination-item"></span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
