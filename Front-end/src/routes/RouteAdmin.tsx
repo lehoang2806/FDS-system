d83ae2a3c4baf18609503ed235f95a000c889d86
tuanpcl7103 <tuanpcl7103.gmail.com>
2025-03-02 19:31:39 +0700

    [ADD]: Admin Campaign

--- Front-end/src/routes/RouteAdmin.tsx
+++ Front-end/src/routes/RouteAdmin.tsx
@@ -1,5 +1,7 @@
 import AdminBasicLayout from "@/layout/Admin/AdminBasicLayout";
 import { AdminDashboardPage } from "@/pages/Admin";
+import { AdminDetailCampaignDonorPage, AdminListCampaignDonorPage } from "@/pages/Admin/Campaign/Donor";
+import { AdminDetailCampaignStaffPage, AdminListCampaignStaffPage } from "@/pages/Admin/Campaign/Staff";
 import { AdminAddStaffPage, AdminDetailStaffPage, AdminListStaffPage } from "@/pages/Admin/Staff";
 import { RouteObject } from "react-router-dom";
 
@@ -23,7 +25,23 @@ const routeAdmin: RouteObject[] = [
             {
                 path: "staff/:id/detail",
                 element: <AdminDetailStaffPage/>
-            }
+            },
+            {
+                path: "campaign/staff",
+                element: <AdminListCampaignStaffPage/>
+            },
+            {
+                path: "campaign/staff/:id/detail",
+                element: <AdminDetailCampaignStaffPage/>
+            },
+            {
+                path: "campaign/donor",
+                element: <AdminListCampaignDonorPage/>
+            },
+            {
+                path: "campaign/donor/:id/detail",
+                element: <AdminDetailCampaignDonorPage/>
+            },
         ]
     }
 ]
