02b746fddada895100b072447e4e45e4cd3715e0
lehoang2806 <lehoang28062001@gmail.com>
2025-03-13 22:40:46 +0700

    updatelogin

---
+++ Back-end/FDSSYSTEM/DTOs/RegisterUserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FDSSYSTEM.DTOs
+{
+    public class RegisterUserDto
+    {
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public int RoleId { get; set; }
+        public bool IsConfirm { get; set; }
+        public string type { get; set; }
+    }
+}
