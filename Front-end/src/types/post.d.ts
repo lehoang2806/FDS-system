85a46d0be7ad9180b9a891ca3f59d968400ec683
pcltuan7103 <tuanpcl7103@gmail.com>
2025-04-16 20:23:11 +0700

    [ADD]

--- Front-end/src/types/post.d.ts
+++ Front-end/src/types/post.d.ts
@@ -5,3 +5,19 @@ interface ActionParamPost {
     posterName?: string;
     posterRole?: string;
 }
+
+interface PostState {
+    listPosts: Post[];
+}
+
+interface Post {
+    postContent: string;
+    images: string[];
+    posterId: string;
+    posterName: string;
+    posterRole: string;
+    status: string;
+    rejectComment: string | null;
+    posterApproverId: string | null;
+    posterApproverName: string | null;
+}
